a46ca97709046c7f302bfe2d6f9c6dddb19f4023
joseph-w-e <joey.w.elliott@gmail.com>
2016-09-21 23:24:11 -0500

    Slightly modified dialog for better styling

--- app/src/main/java/com/androiddev/josephelliott/workoutcalendar/Activities/HomePage/CalendarActivity.java
+++ app/src/main/java/com/androiddev/josephelliott/workoutcalendar/Activities/HomePage/CalendarActivity.java
@@ -106,6 +106,14 @@ public class CalendarActivity extends FragmentActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        if (fabMain != null) {
+            fabMain.close(true);
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_calendar, menu);
