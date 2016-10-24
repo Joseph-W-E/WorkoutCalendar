3bc38db3fae73f4f7d9d5f0ccf7b3854173db3ca
joseph-w-e <joey.w.elliott@gmail.com>
2016-10-24 11:52:05 -0500

    Modified login screen slightly

--- app/src/main/java/com/androiddev/josephelliott/workoutcalendar/Activities/SplashScreen/Splash.java
+++ app/src/main/java/com/androiddev/josephelliott/workoutcalendar/Activities/SplashScreen/Splash.java
@@ -1,10 +1,12 @@
 package com.androiddev.josephelliott.workoutcalendar.Activities.SplashScreen;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.androiddev.josephelliott.workoutcalendar.Activities.Home.CalendarActivity;
 import com.androiddev.josephelliott.workoutcalendar.R;
@@ -14,16 +16,20 @@ import com.androiddev.josephelliott.workoutcalendar.R;
  */
 public class Splash extends Activity {
 
-    /**
-     * Components*/
-    private Button btnUseLocal;
+    /*** Context ***/
+    private Context context;
+
+    /*** Views ***/
+    private Button btnLocalStorage;
+    private Button btnLogIn;
+    private Button btnSignUp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-        btnUseLocal = (Button) findViewById(R.id.splash_btn_local_storage);
+        context = this;
 
         try {
             getActionBar().setElevation(0);
@@ -31,13 +37,42 @@ public class Splash extends Activity {
             // Do nothing
         }
 
-        btnUseLocal.setOnClickListener(new View.OnClickListener() {
+        initializeLocalStorageButton();
+        initializeLogInButton();
+        initializeSignUpButton();
+    }
+
+    private void initializeLocalStorageButton() {
+        btnLocalStorage = (Button) findViewById(R.id.splash_btn_local_storage);
+
+        btnLocalStorage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(Splash.this, CalendarActivity.class));
                 finish();
             }
         });
+    }
 
+    private void initializeLogInButton() {
+        btnLogIn = (Button) findViewById(R.id.splash_btn_log_in);
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Sorry, this button doesn't work yet :/", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initializeSignUpButton() {
+        btnSignUp = (Button) findViewById(R.id.splash_btn_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Sorry, this button doesn't work yet :/", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
